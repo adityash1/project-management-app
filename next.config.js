c7809edf67ce152bca5d0ce0bb619bc5122faa74
adityash1 <aditya167411@gmail.com>
2023-04-15 17:25:51 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    eslint: {
+      ignoreBuildErrors: true,
+    },
     typescript: {
       ignoreBuildErrors: true,
     },
